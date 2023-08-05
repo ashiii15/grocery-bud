25c54e4d6f58f7c9a286ef38ad89cf3ba9edd31c
ashik pa <ashikpa003@gmail.com>
2023-08-05 21:51:06 +0400

    adding clear button

--- app.js
+++ app.js
@@ -18,9 +18,29 @@ form.addEventListener("submit",(e)=>{
     e.preventDefault()
     let data = grocery.value
     console.log(data);
-    let uniqueId = new Date().getTime().toString
+    let Id = new Date().getTime().toString
     if(data && !editFlag){
-        console.log("display th elist");
+        const element = document.createElement("article")
+        element.classList.add("grocery-items")
+        // add id
+        const attr = document.createAttribute("data-id")
+        attr.value = Id
+        element.setAttributeNode(attr)
+        element.innerHTML = `<p class="title">${data}</p>
+        <div class="btn-container">
+          <button type="button" class="edit-btn">
+            <i class="fas fa-edit"></i>
+          </button>
+          <button type="button" class="delete-btn">
+            <i class="fas fa-trash"></i>
+          </button>
+        </div> `
+        lists.appendChild(element)
+        // display
+        container.classList.add("show-container")
+        displayAlert(`item added successfully`,`success`)
+
+
     }
     else if(data && editFlag){
         console.log("editing on progresss");
@@ -36,11 +56,33 @@ function displayAlert(item,action){
     alert.classList.add(`alert-${action}`)
     setTimeout(()=>{
         alert.textContent = ""
+        // remove items
         alert.classList.remove(`alert-${action}`)
     },1000)
 
 }
-s
+clearBtn.addEventListener("click",()=>{
+    const items =document.querySelectorAll(".grocery-items")
+    if(items.length > 0){
+        items.forEach((item)=>{
+            lists.removeChild(item)
+            lists.classList.remove("grocery-items")
+            container.classList.remove("show-container")
+            
+        })
+    }
+
+})
+
+// setback default
+function setbackToDefault(){
+    grocery.value = ""
+    editFlag = false
+    editId = ""
+    submitBtn.textContent = "submit"
+}
+setbackToDefault()
+
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
